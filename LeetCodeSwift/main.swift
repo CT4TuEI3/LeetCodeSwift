a08fe9ebf07fcca35fb31c066fdbbfe5fff9a49f
CT4TuEI3 <alexstatiev@gmail.com>
2025-04-02 07:09:39 +0300

    1768. Merge Strings Alternately

--- LeetCodeSwift/main.swift
+++ LeetCodeSwift/main.swift
@@ -10,18 +10,26 @@ import Foundation
 
 struct Solution {
     
-    // 28. Find the Index of the First Occurrence in a String
+    // 1768. Merge Strings Alternately
     
-    func strStr(_ haystack: String, _ needle: String) -> Int {
-        guard haystack.contains(needle) else { return -1 }
-        if let range = haystack.firstRange(of: needle) {
-            let startIndex = haystack.distance(from: haystack.startIndex, to: range.lowerBound)
-            return startIndex
-        } else {
-            return 0
+    func mergeAlternately(_ word1: String, _ word2: String) -> String {
+        var result = ""
+        let word1Chars = Array(word1)
+        let word2Chars = Array(word2)
+        let maxLength = max(word1Chars.count, word2Chars.count)
+        
+        for i in 0 ..< maxLength {
+            if i < word1Chars.count {
+                result.append(word1Chars[i])
+            }
+            if i < word2Chars.count {
+                result.append(word2Chars[i])
+            }
         }
+        
+        return result
     }
 }
 
 let test = Solution()
-print(test.strStr("sadbutsad", "sad"))
+print(test.mergeAlternately("a   b   c", "    p   q   r"))
