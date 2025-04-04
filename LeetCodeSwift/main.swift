47e0573495567933404de382ff88a668b9f2bdf0
CT4TuEI3 <alexstatiev@gmail.com>
2025-04-04 09:52:17 +0300

    605. Can Place Flowers

--- LeetCodeSwift/main.swift
+++ LeetCodeSwift/main.swift
@@ -8,15 +8,51 @@
 
 import Foundation
 
-struct Solution {
+class Solution {
     
-    // 1431. Kids With the Greatest Number of Candies
+    // 605. Can Place Flowers
     
-    func kidsWithCandies(_ candies: [Int], _ extraCandies: Int) -> [Bool] {
-        guard let maxCandies = candies.max() else { return [] }
-        return candies.map { $0 + extraCandies >= maxCandies }
+    func canPlaceFlowers(_ flowerbed: [Int], _ n: Int) -> Bool {
+        if n == 0 {
+            return true
+        }
+        
+        var i = 0
+        let length = flowerbed.count
+        var maxFlowers = 0
+        
+        while i < length {
+            if flowerbed[i] == 0 {
+                let start = i
+                while i < length && flowerbed[i] == 0 {
+                    i += 1
+                }
+                let end = i - 1
+                let L = end - start + 1
+                
+                let leftOccupied = start > 0 ? (flowerbed[start - 1] == 1) : false
+                let rightOccupied = end < length - 1 ? (flowerbed[end + 1] == 1) : false
+                
+                var count = 0
+                if leftOccupied && rightOccupied {
+                    count = (L - 1) / 2
+                } else if !leftOccupied && !rightOccupied {
+                    count = (L + 1) / 2
+                } else {
+                    count = L / 2
+                }
+                
+                maxFlowers += count
+                if maxFlowers >= n {
+                    return true
+                }
+            } else {
+                i += 1
+            }
+        }
+        
+        return maxFlowers >= n
     }
 }
-
 let test = Solution()
-print(test.kidsWithCandies([2,3,5,1,3], 3))
+print(test.canPlaceFlowers([1,0,0,0,1], 1))
