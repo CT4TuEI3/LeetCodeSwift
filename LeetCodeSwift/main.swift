1f3a7057ae78ebe67ba9e3e55062ac409b8da7f7
CT4TuEI3 <alexstatiev@gmail.com>
2025-04-14 06:41:33 +0300

    1534. Count Good Triplets

--- LeetCodeSwift/main.swift
+++ LeetCodeSwift/main.swift
@@ -10,31 +10,30 @@ import Foundation
 
 final class Solution {
     
-    // 11. Container With Most Water
+    // 1534. Count Good Triplets
     
-    func maxArea(_ height: [Int]) -> Int {
-        var maxWater = 0
-        var left = 0
-        var right = height.count - 1
+    func countGoodTriplets(_ arr: [Int], _ a: Int, _ b: Int, _ c: Int) -> Int {
+        var count = 0
+        let n = arr.count
         
-        while left < right {
-            let currentHeight = min(height[left], height[right])
-            let currentWidth = right - left
-            let currentArea = currentHeight * currentWidth
-            
-            maxWater = max(maxWater, currentArea)
-            
-            if height[left] < height[right] {
-                left += 1
-            } else {
-                right -= 1
+        for i in 0..<n {
+            for j in i+1..<n {
+                for k in j+1..<n {
+                    let condition1 = abs(arr[i] - arr[j]) <= a
+                    let condition2 = abs(arr[j] - arr[k]) <= b
+                    let condition3 = abs(arr[i] - arr[k]) <= c
+                    
+                    if condition1 && condition2 && condition3 {
+                        count += 1
+                    }
+                }
             }
         }
         
-        return maxWater
+        return count
     }
 }
 
 let test = Solution()
 
-print(test.maxArea([1,8,6,2,5,4,8,3,7]))
+print(test.countGoodTriplets([1,8,6,2,5,4,8,3,7], 7, 0, 3))
