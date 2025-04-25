f79133b6840679109c24c385e11e83053c931eeb
CT4TuEI3 <alexstatiev@gmail.com>
2025-04-25 19:12:26 +0300

    1657. Determine if Two Strings Are Close

--- LeetCodeSwift/main.swift
+++ LeetCodeSwift/main.swift
@@ -10,27 +10,38 @@ import Foundation
 
 final class Solution {
     
-    // 1207. Unique Number of Occurrences
+    // 1657. Determine if Two Strings Are Close
     
-    func uniqueOccurrences(_ arr: [Int]) -> Bool {
-        var frequencyDict = [Int: Int]()
-        
-        for num in arr {
-            frequencyDict[num] = (frequencyDict[num] ?? 0) + 1
+    func closeStrings(_ word1: String, _ word2: String) -> Bool {
+        if word1.count != word2.count {
+            return false
         }
         
-        var frequencySet = Set<Int>()
-        for (_, count) in frequencyDict {
-            if frequencySet.contains(count) {
-                return false
-            }
-            frequencySet.insert(count)
+        let word1Array = Array(word1)
+        let word2Array = Array(word2)
+        
+        var freq1 = [Character: Int]()
+        var freq2 = [Character: Int]()
+        
+        for char in word1Array {
+            freq1[char] = (freq1[char] ?? 0) + 1
         }
         
-        return true
+        for char in word2Array {
+            freq2[char] = (freq2[char] ?? 0) + 1
+        }
+        
+        if Set(freq1.keys) != Set(freq2.keys) {
+            return false
+        }
+        
+        let sortedFreq1 = freq1.values.sorted()
+        let sortedFreq2 = freq2.values.sorted()
+        
+        return sortedFreq1 == sortedFreq2
     }
 }
 
 let test = Solution()
 
-print(test.uniqueOccurrences([1,2,2,1,1,3]))
+print(test.closeStrings("abc", "bca"))
